77c2b0207602a74ab257730354454a2a74194fb5
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-03 10:51:55 +0000

    fixes Exercise 1

--- exercises/counter.js
+++ exercises/counter.js
@@ -1,7 +1,14 @@
 // Make your Counter class here
+class Counter {
+    constructor() {
+    this.count = 0
+    }
+}
 
 // Replace `null` with an _instance_ of your counter
-const counterInstance = null
+const counterInstance = new Counter()
 
 // Please don't change the lines below!
 export { Counter, counterInstance }
+
+console.log(counterInstance)
