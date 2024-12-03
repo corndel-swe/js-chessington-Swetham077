132dab93b863f3aedf003bb0cde2d5479a8ef5d4
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-03 11:09:23 +0000

    fixes Exercise 2

--- exercises/counter.js
+++ exercises/counter.js
@@ -3,6 +3,16 @@ class Counter {
     constructor() {
     this.count = 0
     }
+increment() {
+    this.count +=1
+}
+
+reset() {
+    this.count = 0
+}
+getCount() {
+    return this.count
+}
 }
 
 // Replace `null` with an _instance_ of your counter
