d4d8679cc8df2c62bdbc5608bb79dc8f80affc0e
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-04 13:41:18 +0000

    testing

--- src/models/pieces/bishop.js
+++ src/models/pieces/bishop.js
@@ -1,53 +1,47 @@
 import Square from '../square.js'
 import Piece from './piece.js'
+import King from './king.js'
 
-export default class Bishop {
+class Bishop extends Piece {
   constructor(player) {
-    this.player = player
+    super(player)
   }
 
   getAvailableMoves(board) {
-    let location = board.findPiece(this);
-    let moves = [];
+    const loc = board.findPiece(this)
+    const moves = []
 
-    // Define the four diagonal directions
-    const directions = [
-        { row: 1, col: 1 },   // Top-right
-        { row: 1, col: -1 },  // Top-left
-        { row: -1, col: 1 },  // Bottom-right
-        { row: -1, col: -1 }  // Bottom-left
-    ];
+    const dirs = [
+      { dr: 1, dc: 1 },
+      { dr: 1, dc: -1 },
+      { dr: -1, dc: 1 },
+      { dr: -1, dc: -1 }
+    ]
 
-    // Loop through each direction
-    for (let direction of directions) {
-        let row = location.row + direction.row;
-        let col = location.col + direction.col;
+    for (let { dr, dc } of dirs) {
+      let candidate = new Square(loc.row + dr, loc.col + dc)
+      while (board.contains(candidate)) {
+        // Check if there is a piece in the way
+        const capturable = board.getPiece(candidate)
 
-        // Continue moving in the current direction
-        while (row >= 0 && row < 8 && col >= 0 && col < 8) {
-            let square = new Square(row, col);
-            let piece = board.getPiece(square);
-
-            if (board.isOccupied(square)) {
-                if (board.getPiece(square).player !== this.player) {
-                    moves.push(square); // Capture opponent's piece
-                }
-                break; // Stop if a piece is encountered
-            } else {
-                moves.push(square); // Add empty square as a valid move
-            }
-
-            row += direction.row;
-            col += direction.col;
+        if (capturable) {
+          if (
+            capturable.player !== this.player &&
+            !(capturable instanceof King)
+          ) {
+            moves.push(candidate)
+          }
+          break
         }
+
+        moves.push(candidate)
+
+        candidate = new Square(candidate.row + dr, candidate.col + dc)
+      }
     }
 
-    return moves;
+    return moves
+  }
 }
 
-moveTo(board, newSquare) {
-    const currentSquare = board.findPiece(this);
-    board.movePiece(currentSquare, newSquare);
-}
-
-}
+export default Bishop
