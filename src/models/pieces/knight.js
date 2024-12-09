26741ce5482cb2c430f8aa37cb23920d10bce07b
Swetha <swetha.muralitharan@axians.co.uk>
2024-12-09 12:04:15 +0000

    gets pieces to move

--- src/models/pieces/knight.js
+++ src/models/pieces/knight.js
@@ -1,17 +1,47 @@
-import Square from '../square.js'
-import Piece from './piece.js'
+import Square from '../square.js';
+import Piece from './piece.js';
 
 export default class Knight {
   constructor(player) {
-    this.player = player
+    this.player = player;
   }
 
   getAvailableMoves(board) {
-    return []
+    let location = board.findPiece(this);
+    let moves = [];
+
+    const directions = [
+      { row: 2, col: 1 },   // Up-right
+      { row: 2, col: -1 },  // Up-left
+      { row: -2, col: 1 },  // Down-right
+      { row: -2, col: -1 }, // Down-left
+      { row: 1, col: 2 },   // Right-up
+      { row: 1, col: -2 },  // Right-down
+      { row: -1, col: 2 },  // Left-up
+      { row: -1, col: -2 }  // Left-down
+    ];
+
+    // Loop through each direction
+    for (let direction of directions) {
+      let row = location.row + direction.row;
+      let col = location.col + direction.col;
+
+      // Check if the move is within the board boundaries
+      if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+        let square = new Square(row, col);
+        let piece = board.getPiece(square);
+
+        if (!piece || piece.player !== this.player) {
+          moves.push(square); // Add the move if the square is empty or contains an opponent's piece
+        }
+      }
+    }
+
+    return moves;
   }
 
   moveTo(board, newSquare) {
-    const currentSquare = board.findPiece(this)
-    board.movePiece(currentSquare, newSquare)
+    const currentSquare = board.findPiece(this);
+    board.movePiece(currentSquare, newSquare);
   }
 }
